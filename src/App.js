d3997ae0218b112c6ad74e75829413748c8db1c5
Lisa Bolmsten <lisa@bolmsten.se>
2018-02-13 21:28:32 +0100

    Support going to native browser full screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import Fullscreen from 'react-full-screen';
+
 import Track1 from './Track1';
 import Track2 from './Track2';
 import Navigation from './Navigation';
 
 class App extends Component {
+	state = { isFull: false };
+
+	goFull = () => {
+		this.setState({ isFull: true });
+	};
+
 	render() {
 		return (
-			<Router>
-				<Fragment>
-					<Route path="/" exact component={Navigation} />
-					<Route path="/Track1" component={Track1} />
-					<Route path="/Track2" component={Track2} />
-				</Fragment>
-			</Router>
+			<Fullscreen
+				enabled={this.state.isFull}
+				onChange={isFull => this.setState({ isFull })}
+			>
+				<Router>
+					<Fragment>
+						<Route path="/" exact component={Navigation} />
+						<Route path="/Track1" component={Track1} />
+						<Route path="/Track2" component={Track2} />
+
+						<button onClick={this.goFull}>Fullskärm</button>
+					</Fragment>
+				</Router>
+			</Fullscreen>
 		);
 	}
 }
