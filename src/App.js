ab3549eb9e0e2458fa05739b56bf18347573ab5b
Lisa Bolmsten <lisa@bolmsten.se>
2018-02-19 23:57:33 +0100

    Add videos

--- src/App.js
+++ src/App.js
@@ -18,6 +18,43 @@ import scenPlay12 from './videos/scen/scen-play1_2.mp4';
 import scenPlay13 from './videos/scen/scen-play1_3.mp4';
 import scenBild from './videos/scen/scenBild.png';
 
+import Brandstation from './videos/Brandstation.mp4';
+import BrandstationLjud from './videos/BrandstationLjud.mp4';
+
+import Space from './videos/Space.mp4';
+import SpaceRocketArriving from './videos/SpaceRocketArriving.mp4';
+import SpaceRocketStanding from './videos/SpaceRocketStanding.mp4';
+import SpaceRocketLeaving from './videos/SpaceRocketLeaving.mp4';
+import spaceBild from './videos/spaceBild.png';
+
+import Searching from './videos/Searching.mp4';
+import SearchingBase from './videos/SearchingBase.mp4';
+import searchingBild from './videos/searchingBild.png';
+
+import Road1 from './videos/Road1.mp4';
+import Road2 from './videos/Road2.mp4';
+
+import dinos from './videos/dinos.mp4';
+import DinoArriving from './videos/DinoArriving.mp4';
+import DinoStanding from './videos/DinoStanding.mp4';
+import DinoLeaving from './videos/DinoLeaving.mp4';
+import dinosBild from './videos/dinosBild.png';
+
+import dragon from './videos/dragon.mp4';
+import DragonArriving from './videos/DragonArriving.mp4';
+import DragonFire from './videos/DragonFire.mp4';
+import DragonFireEnding from './videos/DragonFireEnding.mp4';
+import dragonBild from './videos/dragonBild.png';
+
+import hastedit from './videos/hastedit.mp4';
+import honsedit from './videos/honsedit.mp4';
+import hundaredit from './videos/hundaredit.mp4';
+
+import circus from './videos/circus.mp4';
+import circusDog from './videos/circusDog.mp4';
+import circusDogSitting from './videos/circusDogSitting.mp4';
+import circusBild from './videos/circusBild.png';
+
 //structure of videos
 const tracks = [
 	{
@@ -32,6 +69,69 @@ const tracks = [
 			{ src: scenPlay12, loop: true },
 			{ src: scenPlay13, loop: false },
 		],
+	},
+	{
+		poster: circusBild,
+		videos: [
+			{ src: circus, loop: true },
+			{ src: circusDog, loop: false },
+			{ src: circusDogSitting, loop: true },
+			{ src: circus, loop: true },
+		],
+	},
+	{
+		poster: dragonBild,
+		videos: [
+			{ src: dragon, loop: true },
+			{ src: DragonArriving, loop: false },
+			{ src: DragonFire, loop: true },
+			{ src: DragonFireEnding, loop: false },
+			{ src: dragon, loop: true },
+		],
+	},
+	{
+		poster: searchingBild,
+		videos: [
+			{ src: SearchingBase, loop: true },
+			{ src: Searching, loop: true },
+			{ src: SearchingBase, loop: true },
+		],
+	},
+	{
+		poster: spaceBild,
+		videos: [
+			{ src: Space, loop: true },
+			{ src: SpaceRocketArriving, loop: false },
+			{ src: SpaceRocketStanding, loop: true },
+			{ src: SpaceRocketLeaving, loop: false },
+			{ src: Space, loop: true },
+		],
+	},
+	{
+		poster: dinosBild,
+		videos: [
+			{ src: dinos, loop: true },
+			{ src: DinoArriving, loop: false },
+			{ src: DinoStanding, loop: true },
+			{ src: DinoLeaving, loop: false },
+			{ src: dinos, loop: true },
+		],
+	},
+	{
+		poster: '',
+		videos: [{ src: Brandstation, loop: true }, { src: BrandstationLjud, loop: true }],
+	},
+	{
+		poster: '',
+		videos: [
+			{ src: hastedit, loop: true },
+			{ src: honsedit, loop: true },
+			{ src: hundaredit, loop: true },
+		],
+	},
+	{
+		poster: '',
+		videos: [{ src: Road2, loop: true }, { src: Road1, loop: true }],
 	},
 ];
 
